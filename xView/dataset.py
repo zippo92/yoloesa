7e98d676e3965cdbd901c57d05ddfb9c9adaecb8
zippo92 <simone.zippo@gmail.com>
2018-07-23 14:20:57 +0200

    prova

--- xView/dataset.py
+++ xView/dataset.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import numpy as np
 import os
 
+
 class Dataset(object):
     def __init__(self, file_path):
         self.path = file_path
@@ -15,31 +16,72 @@ class Dataset(object):
     def build(self, num_class=10,
               height=256, width=256,
               batch_size=32, num_epochs=100,
-              shuffle=1000000, num_parallel_calls=2):
+              shuffle=1000000, num_parallel_calls=2, s=9, b=4):
         self._num_class = num_class
         self._height = height
         self._width = width
         self._batch_size = batch_size
         self._num_epochs = num_epochs
         self._shuffle = shuffle
+        self._s = s
+        self._b = b
         # self.dataset = self.dataset.shuffle(self._shuffle)
         self.dataset = self.dataset.map(self.__input_parser, num_parallel_calls=num_parallel_calls)
-        #self.dataset = self.dataset.padded_batch(self._batch_size, padded_shapes= [None,None,None])
-	self.dataset = self.dataset.apply(tf.contrib.data.batch_and_drop_remainder(self._batch_size))
+        # self.dataset = self.dataset.padded_batch(self._batch_size, padded_shapes= [None,None,None])
+        self.dataset = self.dataset.apply(tf.contrib.data.batch_and_drop_remainder(self._batch_size))
         # self.dataset = self.dataset.repeat(self._num_epochs)
-        #self._iterator = tf.data.Iterator.from_structure(self.dataset.output_types,
-#                                                        self.dataset.output_shapes)
-       
-	self._iterator = self.dataset.make_initializable_iterator()
+        # self._iterator = tf.data.Iterator.from_structure(self.dataset.output_types,
+        #                                                        self.dataset.output_shapes)
+
+        self._iterator = self.dataset.make_initializable_iterator()
 
     def get_next(self):
         return self._iterator.get_next()
 
     def init(self):
-        #return self._iterator.make_initializer(self.dataset)
-	return self._iterator.initializer
-    def __input_parser(self, example):
+        # return self._iterator.make_initializer(self.dataset)
+        return self._iterator.initializer
 
+    def __parse_bb(self,tensor):
+        xmin = tensor[0]
+        xmax = tensor[1]
+        ymin = tensor[2]
+        ymax = tensor[3]
+        label = tensor[4]
+
+        width = tf.subtract(xmax, xmin)
+        height = tf.subtract(ymax, ymin)
+
+        # acnhor_indxs = self.get_active_anchors(width, height)
+
+        return width, height
+    def get_active_anchors(self, w, h):
+        indxs = []
+        iou_max, index_max = 0, 0
+        for i, a in enumerate(self.anchors):
+            iou = self.iou_wh([w,h], a)
+            if iou > 0.7:
+                indxs.append(i)
+            if iou > iou_max:
+                iou_max, index_max = iou, i
+
+        if len(indxs) == 0:
+            indxs.append(index_max)
+
+        return indxs
+
+    def iou_wh(self, r1, r2):
+        min_w = tf.minimum(r1[0], r2[0])
+        min_h = tf.minimum(r1[1], r2[1])
+        area_r1 = r1[0] * r1[1]
+        area_r2 = r2[0] * r2[1]
+
+        intersect = min_w * min_h
+        union = area_r1 + area_r2 - intersect
+
+        return intersect / union
+
+    def __input_parser(self, example):
         read_features = {
             'image/height': tf.FixedLenFeature((), dtype=tf.int64),
             'image/width': tf.FixedLenFeature((), dtype=tf.int64),
@@ -51,19 +93,22 @@ class Dataset(object):
             'image/object/bbox/ymax': tf.VarLenFeature(tf.float32),
             'image/object/class/label': tf.VarLenFeature(tf.int64)}
 
+        zeros = tf.zeros(shape=[self._s,self._s,self._b*4+self._num_class])
         parsed = tf.parse_single_example(example, features=read_features)
 
         img = tf.image.decode_image(parsed['image/encoded'])
-       # img = tf.image.decode_jpeg(parsed['image/encoded'])
+        # img = tf.image.decode_jpeg(parsed['image/encoded'])
         width = parsed['image/width']
         height = parsed['image/height']
         format = parsed['image/format']
-        xmin = tf.sparse_tensor_to_dense(parsed['image/object/bbox/xmin'])
-        xmax = tf.sparse_tensor_to_dense(parsed['image/object/bbox/xmax'])
-        ymin = tf.sparse_tensor_to_dense(parsed['image/object/bbox/ymin'])
-        ymax = tf.sparse_tensor_to_dense(parsed['image/object/bbox/ymax'])
+        bb_xmin = tf.sparse_tensor_to_dense(parsed['image/object/bbox/xmin'])
+        bb_xmax = tf.sparse_tensor_to_dense(parsed['image/object/bbox/xmax'])
+        bb_ymin = tf.sparse_tensor_to_dense(parsed['image/object/bbox/ymin'])
+        bb_ymax = tf.sparse_tensor_to_dense(parsed['image/object/bbox/ymax'])
         label = tf.sparse_tensor_to_dense(parsed['image/object/class/label'])
-	print(label)
-       # bbox = [[xmin[i]/width,xmax[i]/width,ymin[i]/height,ymax[i]/height] for i in range(xmin.shape[0])]
-	
-        return img , xmin, label
+
+        bb = tf.stack([bb_xmin,bb_xmax,bb_ymin,bb_ymax,label], axis = 1)
+
+        w,h = tf.map_fn(self.__parse_bb, bb)
+
+        return img, w, label
