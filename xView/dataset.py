9352b6aeba15b8f54c6b22e4f6bc37342e966adf
root <root@kickseed.cf>
2018-07-23 11:09:16 +0200

    prova

--- xView/dataset.py
+++ xView/dataset.py
@@ -24,17 +24,20 @@ class Dataset(object):
         self._shuffle = shuffle
         # self.dataset = self.dataset.shuffle(self._shuffle)
         self.dataset = self.dataset.map(self.__input_parser, num_parallel_calls=num_parallel_calls)
-        self.dataset = self.dataset.apply(tf.contrib.data.batch_and_drop_remainder(self._batch_size))
+        #self.dataset = self.dataset.padded_batch(self._batch_size, padded_shapes= [None,None,None])
+	self.dataset = self.dataset.apply(tf.contrib.data.batch_and_drop_remainder(self._batch_size))
         # self.dataset = self.dataset.repeat(self._num_epochs)
-        self._iterator = tf.data.Iterator.from_structure(self.dataset.output_types,
-                                                         self.dataset.output_shapes)
+        #self._iterator = tf.data.Iterator.from_structure(self.dataset.output_types,
+#                                                        self.dataset.output_shapes)
+       
+	self._iterator = self.dataset.make_initializable_iterator()
 
     def get_next(self):
         return self._iterator.get_next()
 
     def init(self):
-        return self._iterator.make_initializer(self.dataset)
-
+        #return self._iterator.make_initializer(self.dataset)
+	return self._iterator.initializer
     def __input_parser(self, example):
 
         read_features = {
@@ -42,25 +45,25 @@ class Dataset(object):
             'image/width': tf.FixedLenFeature((), dtype=tf.int64),
             'image/encoded': tf.FixedLenFeature((), dtype=tf.string),
             'image/format': tf.FixedLenFeature((), dtype=tf.string),
-            'image/object/bbox/xmin': tf.FixedLenFeature((), dtype=tf.float32),
-            'image/object/bbox/xmax': tf.FixedLenFeature((), dtype=tf.float32),
-            'image/object/bbox/ymin': tf.FixedLenFeature((), dtype=tf.float32),
-            'image/object/bbox/ymax': tf.FixedLenFeature((), dtype=tf.float32),
-            'image/object/class/label': tf.FixedLenFeature((), dtype=tf.int64)}
+            'image/object/bbox/xmin': tf.VarLenFeature(tf.float32),
+            'image/object/bbox/xmax': tf.VarLenFeature(tf.float32),
+            'image/object/bbox/ymin': tf.VarLenFeature(tf.float32),
+            'image/object/bbox/ymax': tf.VarLenFeature(tf.float32),
+            'image/object/class/label': tf.VarLenFeature(tf.int64)}
 
         parsed = tf.parse_single_example(example, features=read_features)
 
-        # img = tf.decode_raw(parsed['image/encoded'], tf.uint8)
-        img = tf.image.decode_jpeg(parsed['image/encoded'])
+        img = tf.image.decode_image(parsed['image/encoded'])
+       # img = tf.image.decode_jpeg(parsed['image/encoded'])
         width = parsed['image/width']
         height = parsed['image/height']
         format = parsed['image/format']
-        xmin = parsed['image/object/bbox/xmin']
-        xmax = parsed['image/object/bbox/xmax']
-        ymin = parsed['image/object/bbox/ymin']
-        ymax = parsed['image/object/bbox/ymax']
-        label = parsed['image/object/class/label']
-
-        # bbox = [xmin,xmax,ymin,ymax]
-
-        return img, xmin, label
+        xmin = tf.sparse_tensor_to_dense(parsed['image/object/bbox/xmin'])
+        xmax = tf.sparse_tensor_to_dense(parsed['image/object/bbox/xmax'])
+        ymin = tf.sparse_tensor_to_dense(parsed['image/object/bbox/ymin'])
+        ymax = tf.sparse_tensor_to_dense(parsed['image/object/bbox/ymax'])
+        label = tf.sparse_tensor_to_dense(parsed['image/object/class/label'])
+	print(label)
+       # bbox = [[xmin[i]/width,xmax[i]/width,ymin[i]/height,ymax[i]/height] for i in range(xmin.shape[0])]
+	
+        return img , xmin, label
--- xView/prova.py
+++ xView/prova.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 from dataset import Dataset
 
@@ -8,14 +8,17 @@ dataset = Dataset("./data/xview_train_t1.record")
 
 dataset.build()
 
+
 img, bbox, label = dataset.get_next()
 
 with tf.Session() as sess:
+    	
     sess.run(dataset.init())
-    _img, _bbox, _label = sess.run([img, bbox, label])
-
-    print(_label[0])
-    print(_bbox[0])
+    for i in range(32):
+    	_img, _bbox, _label = sess.run([img, bbox, label])
+    	print(_label)
+    
+    	print(_bbox)
     # plt.imshow(_img[0])
     # plt.show()
-    print(_img[0].shape)
+   # print(_img[0].shape)
