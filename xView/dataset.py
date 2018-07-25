537064a07c238b9c769a0910a8ee6c2483c38fff
zippo92 <simone.zippo@gmail.com>
2018-07-25 14:39:57 +0200

    boh

--- xView/dataset.py
+++ xView/dataset.py
@@ -3,18 +3,20 @@ from __future__ import absolute_import
 import tensorflow as tf
 import numpy as np
 import os
+import ast
 
 
 class Dataset(object):
-    def __init__(self, file_path):
+    def __init__(self, file_path, anchor_path):
         self.path = file_path
+        self.anchor_path = anchor_path
         self.dataset = tf.data.TFRecordDataset(file_path)
 
     def __len__(self):
         return sum(1 for _ in tf.python_io.tf_record_iterator(self.path))
 
     def build(self, num_class=10,
-              height=256, width=256,
+              height=416, width=416,
               batch_size=32, num_epochs=100,
               shuffle=1000000, num_parallel_calls=2, s=9, b=4):
         self._num_class = num_class
@@ -25,8 +27,11 @@ class Dataset(object):
         self._shuffle = shuffle
         self._s = s
         self._b = b
+        with open(self.anchor_path, 'r') as f:
+            self._anchors = ast.literal_eval(f.read())
         self.dataset = self.dataset.shuffle(self._shuffle)
         self.dataset = self.dataset.map(self.__input_parser, num_parallel_calls=num_parallel_calls)
+
         # self.dataset = self.dataset.padded_batch(self._batch_size, padded_shapes= [None,None,None])
         # self.dataset = self.dataset.apply(tf.contrib.data.batch_and_drop_remainder(self._batch_size))
         # self.dataset = self.dataset.repeat(self._num_epochs)
@@ -47,6 +52,7 @@ class Dataset(object):
         xmax = tensor[1]
         ymin = tensor[2]
         ymax = tensor[3]
+        box_class = tensor[4]
 
         width = tf.subtract(xmax, xmin)
         height = tf.subtract(ymax, ymin)
@@ -54,22 +60,22 @@ class Dataset(object):
         x_center = tf.add(xmin, tf.divide(width,tf.constant(2,dtype=tf.float32)))
         y_center = tf.add(ymin, tf.divide(height,tf.constant(2, dtype=tf.float32)))
 
-        # Bisogna dividere width e height per gli anchor attivi, gli anchor dovranno essere sicuramente in percentuale
-
         # anchor_indxs = self.get_active_anchors(width, height)
         # w_scale = tf.divide(width,anchor[0])
         # h_scale = tf.divide(height,anchor[1])
 
-        return width, height, x_center,y_center #w_scale,h_scale
+        return x_center, y_center, width, height, box_class
 
-    #TODO da fare, l'ho solo copiata
     def get_active_anchors(self, w, h):
-        indxs = []
+        indxs = tf.Variable()
         iou_max, index_max = 0, 0
         for i, a in enumerate(self.anchors):
             iou = self.iou_wh([w,h], a)
-            if iou > 0.7:
-                indxs.append(i)
+
+            indxs = tf.cond(tf.less(tf.constant(0.7,dtype=tf.float32),iou),lambda: tf.concat([indxs,tf.constant(a,dtype=tf.float32)]))
+
+            # if iou > 0.7:
+            #     indxs.append(i)
             if iou > iou_max:
                 iou_max, index_max = iou, i
 
@@ -79,15 +85,15 @@ class Dataset(object):
         return indxs
 
     def iou_wh(self, r1, r2):
-        min_w = tf.minimum(r1[0], r2[0])
-        min_h = tf.minimum(r1[1], r2[1])
-        area_r1 = r1[0] * r1[1]
-        area_r2 = r2[0] * r2[1]
+        min_w = tf.minimum(r1[0], tf.constant(r2[0], dtype=tf.float32))
+        min_h = tf.minimum(r1[1], tf.constant(r2[1], dtype=tf.float32))
+        area_r1 = tf.multiply(r1[0],r1[1])
+        area_r2 = tf.multiply(r2[0],r2[1])
 
-        intersect = min_w * min_h
-        union = area_r1 + area_r2 - intersect
+        intersect = tf.multiply(min_w,min_h)
+        union = tf.subtract(tf.add(area_r1,area_r2),intersect)
 
-        return intersect / union
+        return tf.divide(intersect,union)
 
     def __input_parser(self, example):
         read_features = {
@@ -101,7 +107,6 @@ class Dataset(object):
             'image/object/bbox/ymax': tf.VarLenFeature(tf.float32),
             'image/object/class/label': tf.VarLenFeature(tf.int64)}
 
-        zeros = tf.zeros(shape=[self._s,self._s,self._b*4+self._num_class])
         parsed = tf.parse_single_example(example, features=read_features)
 
         img = tf.image.decode_image(parsed['image/encoded'])
@@ -113,16 +118,36 @@ class Dataset(object):
         bb_ymin = tf.sparse_tensor_to_dense(parsed['image/object/bbox/ymin'])
         bb_ymax = tf.sparse_tensor_to_dense(parsed['image/object/bbox/ymax'])
         label = tf.sparse_tensor_to_dense(parsed['image/object/class/label'])
+        label = tf.cast(label, tf.float32)
 
-        bb = tf.stack([bb_xmin,bb_xmax,bb_ymin,bb_ymax], axis = 1)
+        bb = tf.stack([bb_xmin,bb_xmax,bb_ymin,bb_ymax, label], axis = 1)
+
+        conv_height = self._height // 32
+        conv_width = self._width // 32
+        num_box_params = bb.get_shape()[1]
+        num_anchors = len(self._anchors)
+        detector_mask =tf.zeros([conv_height,conv_width, num_anchors, 1], dtype=tf.float32)
+        matching_true_boxes = tf.zeros([conv_height,conv_width,num_anchors, num_box_params])
+        img = tf.reshape(img,[self._height,self._width,3])
+        img = tf.image.resize_images(img, size = [self._height,self._width])
+
 
         bb = tf.map_fn(self.__parse_bb, bb, dtype = (tf.float32,tf.float32,tf.float32,tf.float32))
 
-        grid_x = tf.multiply(bb[2], tf.constant(self._s, dtype=tf.float32))
-        grid_y = tf.multiply(bb[3], tf.constant(self._s, dtype=tf.float32))
+        grid_x = tf.floor(tf.multiply(bb[0], tf.constant(self._s, dtype=tf.float32)))
+        grid_y = tf.floor(tf.multiply(bb[1], tf.constant(self._s, dtype=tf.float32)))
+        print(grid_x)
+        print(grid_y)
+        # grid_x_offset = tf.subtract(grid_x, tf.round(grid_x))
+        # grid_y_offset = tf.subtract(grid_y, tf.round(grid_y))
 
-        grid_x_offset = tf.subtract(grid_x, tf.round(grid_x))
-        grid_y_offset = tf.subtract(grid_y, tf.round(grid_y))
+        bb = tf.stack([bb[0],bb[1], bb[2], bb[3], bb[4]], axis = 1) #xcenter,ycenter,width,height, class
+
+        # for k, anchor in enumerate(self._anchors):
+        box_maxes = bb[:,2:4] / 2.
+        box_mins = -box_maxes
+        anchor_maxes = (self._anchors[0] / 2.)
+        anchor_mins = -anchor_maxes
 
 
         # Per ogni active anchor
@@ -131,4 +156,4 @@ class Dataset(object):
 
 
 
-        return img, grid_x, grid_y, bb[2], bb[3], label
+        return img, box_mins, anchor_mins, bb[2], bb[3], label
--- xView/prova.py
+++ xView/prova.py
@@ -4,21 +4,19 @@ import numpy as np
 
 from dataset import Dataset
 
-dataset = Dataset("./data/xview_train_t1.record")
+dataset = Dataset("./data/xview_train_t1.record", "./data/xview_anchors")
 
 dataset.build()
-
 
 img, bbox, label = dataset.get_next()
 
 with tf.Session() as sess:
-    	
     sess.run(dataset.init())
     for i in range(32):
-    	_img, _bbox, _label = sess.run([img, bbox, label])
-    	print(_label)
-    
-    	print(_bbox)
+        _img, _bbox, _label = sess.run([img, bbox, label])
+        print(_label)
+
+        print(_bbox)
     # plt.imshow(_img[0])
     # plt.show()
-   # print(_img[0].shape)
+# print(_img[0].shape)
