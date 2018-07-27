01cedb3d2465107917ad4e5c94dcf18ae4cf4cc0
zippo92 <simone.zippo@gmail.com>
2018-07-27 10:10:07 +0200

    test

--- xView/prova.py
+++ xView/prova.py
@@ -8,23 +8,16 @@ dataset = Dataset("./data/xview_train_t1.record", "./data/xview_anchors")
 
 dataset.build()
 
-bb, anchors, iou, iou_max, iou_stack = dataset.get_next()
+bb_hw1, bb_hw2 = dataset.get_next()
 
 with tf.Session() as sess:
     sess.run(dataset.init())
     for i in range(1):
-        _bb, _anchor, _iou, _iou_max, _iou_stack = sess.run([bb,anchors, iou, iou_max, iou_stack])
-        print(_bb.shape)
-        print(_anchor.shape)
-        print(_bb)
-
-        print(_anchor)
-
-        print(_iou)
-
-        print(_iou_max)
-
-        print(_iou_stack)
+        _bbhw1, _bbhw2 = sess.run([bb_hw1, bb_hw2])
+        print(bb_hw1.shape)
+        print(bb_hw1)
+        print(bb_hw2.shape)
+        print(bb_hw2)
     # plt.imshow(_img[0])
     # plt.show()
 # print(_img[0].shape)
